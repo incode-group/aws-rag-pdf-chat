121ef4246cb04baff6630023e6086c80a88f9db9
andrew-dev-p <popov.andrey.dev@gmail.com>
2025-07-24 20:43:58 +0300

    feat: Add routing with react-router and implement plain Login and Chat components

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Chat from './components/Chat'
+import Login from './components/Login'
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+      <Routes>
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
